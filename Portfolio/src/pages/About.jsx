40a9ffa0ea8e46c35fa1321404e2b7cfe5b3e239
Olyad Kitila <olyad987@gmail.com>
2025-01-05 20:24:23 -0500

    Testing proper asset imports

--- Portfolio/src/pages/About.jsx
+++ Portfolio/src/pages/About.jsx
@@ -4,20 +4,20 @@ export default function About() {
       <h1>Art Page</h1>
       <div class="gallery-container">
         <img src="../public/assets/AYE_LandingImage.jpg"></img>
-        <img src="../src/assets/riversea darker.png"></img>
-        <img src="../src/assets/idkkkk.png"></img>
-        <img src="../src/assets/Museum1.png"></img>
-        <img src="../src/assets/Untitled-1.png"></img>
+        <img src="public/assets/riversea darker.png"></img>
+        <img src="../assets/idkkkk.png"></img>
+        <img src="../assets/Museum1.png"></img>
+        <img src="../assets/Untitled-1.png"></img>
         <video playsinline muted loop autoplay="autoplay"  src="../src/assets/warning_left.webm" type="video/mp4"></video>
-        <img src="../src/assets/IMG_2345.JPG"></img>
-        <img src="../src/assets/Group 4csfir.png"></img>
-        <img src="../src/assets/IMG_1753.00_00_00_00.Still001.png"></img>
+        <img src="../assets/IMG_2345.JPG"></img>
+        <img src="../assets/Group 4csfir.png"></img>
+        <img src="../assets/IMG_1753.00_00_00_00.Still001.png"></img>
         <video playsInline muted loop autoplay="autoplay" src="../src/assets/warning_middle.webm"></video>
-        <img src="../src/assets/IMG_1342.jpg"></img>
-        <img src="../src/assets/IMG_4917.JPG"></img>
-        <video playsInline muted loop autoplay="autplay" src="../src/assets/warning_right.webm" type="video/mp4" ></video>
-        <video muted loop autoplay="autoplay" src="../src/assets/cisse, malian women.mp4"></video>
-        <img src="../src/assets/110277014_287679179008477_7462832936245290786_n.jpeg"></img>
+        <img src="../assets/IMG_1342.jpg"></img>
+        <img src="../assets/IMG_4917.JPG"></img>
+        <video playsInline muted loop autoplay="autplay" src="../assets/warning_right.webm" type="video/mp4" ></video>
+        <video muted loop autoplay="autoplay" src="../assets/cisse, malian women.mp4"></video>
+        <img src="../assets/110277014_287679179008477_7462832936245290786_n.jpeg"></img>
       </div>
     </main>
   );
