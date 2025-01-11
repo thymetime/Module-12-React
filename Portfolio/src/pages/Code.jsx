76c8467ab88ca5ce3c847722b971753e010092ae
Olyad Kitila <olyad987@gmail.com>
2025-01-11 00:16:46 -0500

    corrected project links

--- Portfolio/src/pages/Code.jsx
+++ Portfolio/src/pages/Code.jsx
@@ -4,34 +4,34 @@ export default function Code() {
         <h2 class="proj-intro" id="proj-intro"><span>My Projects</span></h2>
         <div class="proj-container">
             <a
-            href="https://www.artsy.net/artwork/uta-barth-field-number-9"
+            href="car-patron-app.onrender.com"
             class="proj-link"
             id="proj-link-1">
-                <img src="../assets/coding/carpatron_ss" alt="Amy Doing Makeup" class="proj-img" />
+                <img src="../assets/coding/carpatron_ss" alt="CarPatron | Keep track of your car's maintenance" class="proj-img" />
                 <p class="proj-title"><span>CarPatron</span></p>
             </a>
 
             <a
-            href="Quid-pro-quo.html"
+            href="https://amygdala.onrender.com/"
             class="proj-link"
             id="proj-link-2">
-                <img src="../assets/coding/amygdala" alt="Amy Doing Makeup" class="proj-img" />
+                <img src="../assets/coding/amygdala" alt="Amygdala | Mental Health Tracking App" class="proj-img" />
                 <p class="proj-title"><span>Amygdala | Mood Tracker</span></p>
             </a>
 
             <a
-            href="navbar.html"
+            href="https://module-9-servers.onrender.com/"
             class="proj-link"
             id="proj-link-3">
-                <img src="../assets/coding/weather-fivecast_ss" alt="Amy Doing Makeup" class="proj-img" />
+                <img src="../assets/coding/weather-fivecast_ss" alt="Weather Fivecast, 5-day forecast for any city" class="proj-img" />
                 <p class="proj-title"><span>Weather Fivecast</span></p>
             </a>
 
             <a
-            href="freecodecamp_technical.html"
+            href="thymetime.github.io/aye"
             class="proj-link"
             id="proj-link-4">
-                <img src="aye_ss" alt="Amy Doing Makeup" class="proj-img" />
+                <img src="aye_ss" alt="Avert Your Eyes website" class="proj-img" />
                 <p class="proj-title"><span>Avert Your Eyes</span></p>
             </a>
 
