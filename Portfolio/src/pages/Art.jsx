b6793c15549120bbc9fbd7cd58b16999a02d8f58
Olyad Kitila <olyad987@gmail.com>
2025-01-13 15:00:30 -0500

    Changed art page title and img margins

--- Portfolio/src/pages/Art.jsx
+++ Portfolio/src/pages/Art.jsx
@@ -1,18 +1,18 @@
 export default function Art() {
   return (
     <main class="art-container">
-      <h1>Art Page</h1>
+      <h2 class="art-title">My Art</h2>
       <div class="gallery-container">
         <img src="../assets/art/AYE_LandingImage.jpg"></img>
         <img src="../assets/art/riversea darker.png"></img>
         <img src="../assets/art/idkkkk.png"></img>
+        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_left.webm" type="video/mp4"></video>
         <img src="../assets/art/Museum1.png"></img>
         <img src="../assets/art/Untitled-1.png"></img>
-        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_left.webm" type="video/mp4"></video>
         <img src="../assets/art/IMG_2345.JPG"></img>
+        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_middle.webm" type="video/mp4"></video>
         <img src="../assets/art/Group 4csfir.png"></img>
         <img src="../assets/art/IMG_1753.00_00_00_00.Still001.png"></img>
-        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_middle.webm" type="video/mp4"></video>
         <img src="../assets/art/IMG_1342.jpg"></img>
         <img src="../assets/art/IMG_4917.JPG"></img>
         <video playsInline muted loop autoplay="autplay" src="../assets/art/warning_right.webm" type="video/mp4" ></video>
