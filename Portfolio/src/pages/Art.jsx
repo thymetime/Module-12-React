b12b90106bed0bc7ccfe81e305be37cc79738f09
Olyad Kitila <olyad987@gmail.com>
2025-01-13 14:51:09 -0500

    Centered art page container, fixed webm src links

--- Portfolio/src/pages/Art.jsx
+++ Portfolio/src/pages/Art.jsx
@@ -8,11 +8,11 @@ export default function Art() {
         <img src="../assets/art/idkkkk.png"></img>
         <img src="../assets/art/Museum1.png"></img>
         <img src="../assets/art/Untitled-1.png"></img>
-        <video playsInline muted loop autoplay="autoplay" src="../src/assets/art/warning_left.webm" type="video/mp4"></video>
+        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_left.webm" type="video/mp4"></video>
         <img src="../assets/art/IMG_2345.JPG"></img>
         <img src="../assets/art/Group 4csfir.png"></img>
         <img src="../assets/art/IMG_1753.00_00_00_00.Still001.png"></img>
-        <video playsInline muted loop autoplay="autoplay" src="../src/assets/art/warning_middle.webm" type="video/mp4"></video>
+        <video playsInline muted loop autoplay="autoplay" src="../assets/art/warning_middle.webm" type="video/mp4"></video>
         <img src="../assets/art/IMG_1342.jpg"></img>
         <img src="../assets/art/IMG_4917.JPG"></img>
         <video playsInline muted loop autoplay="autplay" src="../assets/art/warning_right.webm" type="video/mp4" ></video>
