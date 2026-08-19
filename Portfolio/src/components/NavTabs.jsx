7288cc6a4870ab595afa54feec20bad77f05d487
Olyad Kitila <olyad987@gmail.com>
2026-08-19 13:39:35 -0400

    fix dropdown nav name to account for current page

--- Portfolio/src/components/NavTabs.jsx
+++ Portfolio/src/components/NavTabs.jsx
@@ -35,7 +35,7 @@ function NavTabs() {
               to="/"
               className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
             >
-              {currentPage}.toString().slice(1).toUpperCase();
+              currentPage.toString().slice(1).toUpperCase();
             </Link>            
             <span class='bx bxs-chevron-down htmlcss-arrow arrow  '>v</span>
             {/* nav-dropdown_sub-menu = "htmlCss-sub-menu sub-menu"> */}
